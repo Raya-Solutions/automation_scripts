370455a55a7442f7bcc687ee52d5f130aaeb638a
Ranz Manalo <manaloranzelle10@gmail.com>
2024-09-01 16:03:21 +0800

    g

--- daily_night_diff_test.py
+++ daily_night_diff_test.py
@@ -6,36 +6,36 @@ def insert_employee_checkin():
     # List of all employee check-ins to be inserted TEST
     employee_checkin_data = [
     # Regular Workday before a Regular Holiday (shift from 19:00 to 4:00)
-    {'employee': '22831242', 'time': '2024-12-23 19:00:00', 'log_type': 'IN'},  # Night shift start before Christmas Day (Monday)
-    {'employee': '22831242', 'time': '2024-12-24 04:00:00', 'log_type': 'OUT'},  # Night shift end (Tuesday)
+    {'employee': '22831242', 'time': '2023-12-23 19:00:00', 'log_type': 'IN'},  # Night shift start before Christmas Day (Saturday)
+    {'employee': '22831242', 'time': '2023-12-24 04:00:00', 'log_type': 'OUT'},  # Night shift end (Sunday)
 
     # Regular Holiday Overtime with Attendance on the Previous Day
-    {'employee': '22831242', 'time': '2024-12-24 19:00:00', 'log_type': 'IN'},  # Night shift start on Christmas Day (Tuesday)
-    {'employee': '22831242', 'time': '2024-12-25 05:00:00', 'log_type': 'OUT'},  # Night shift end with 1 hour overtime (Wednesday)
+    {'employee': '22831242', 'time': '2023-12-24 19:00:00', 'log_type': 'IN'},  # Night shift start on Christmas Day (Sunday)
+    {'employee': '22831242', 'time': '2023-12-25 05:00:00', 'log_type': 'OUT'},  # Night shift end with 1 hour overtime (Monday)
 
     # Regular Workday before a Special Non-Working Holiday (shift from 19:00 to 4:00)
-    {'employee': '22831242', 'time': '2024-10-30 19:00:00', 'log_type': 'IN'},  # Night shift start before All Saints' Day (Wednesday)
-    {'employee': '22831242', 'time': '2024-10-31 04:00:00', 'log_type': 'OUT'},  # Night shift end (Thursday)
+    {'employee': '22831242', 'time': '2023-10-30 19:00:00', 'log_type': 'IN'},  # Night shift start before All Saints' Day (Monday)
+    {'employee': '22831242', 'time': '2023-10-31 04:00:00', 'log_type': 'OUT'},  # Night shift end (Tuesday)
 
     # Special Non-Working Holiday Overtime with Attendance on the Previous Day
-    {'employee': '22831242', 'time': '2024-10-31 19:00:00', 'log_type': 'IN'},  # Night shift start on All Saints' Day (Thursday)
-    {'employee': '22831242', 'time': '2024-11-01 06:00:00', 'log_type': 'OUT'},  # Night shift end with 2 hours overtime (Friday)
+    {'employee': '22831242', 'time': '2023-10-31 19:00:00', 'log_type': 'IN'},  # Night shift start on All Saints' Day (Tuesday)
+    {'employee': '22831242', 'time': '2023-11-01 06:00:00', 'log_type': 'OUT'},  # Night shift end with 2 hours overtime (Wednesday)
 
     # Regular Workday before Combined Regular Holiday and Rest Day (shift from 19:00 to 4:00)
-    {'employee': '22831242', 'time': '2024-12-28 19:00:00', 'log_type': 'IN'},  # Night shift start before Rizal Day (Saturday)
-    {'employee': '22831242', 'time': '2024-12-29 04:00:00', 'log_type': 'OUT'},  # Night shift end (Sunday)
+    {'employee': '22831242', 'time': '2023-12-28 19:00:00', 'log_type': 'IN'},  # Night shift start before Rizal Day (Thursday)
+    {'employee': '22831242', 'time': '2023-12-29 04:00:00', 'log_type': 'OUT'},  # Night shift end (Friday)
 
     # Combined Regular Holiday and Rest Day Overtime with Attendance on the Previous Day
-    {'employee': '22831242', 'time': '2024-12-29 19:00:00', 'log_type': 'IN'},  # Night shift start on Rizal Day (Sunday)
-    {'employee': '22831242', 'time': '2024-12-30 07:00:00', 'log_type': 'OUT'},  # Night shift end with 3 hours overtime (Monday)
+    {'employee': '22831242', 'time': '2023-12-29 19:00:00', 'log_type': 'IN'},  # Night shift start on Rizal Day (Friday)
+    {'employee': '22831242', 'time': '2023-12-30 07:00:00', 'log_type': 'OUT'},  # Night shift end with 3 hours overtime (Saturday)
 
     # Regular Workday before Combined Special Non-Working Holiday and Rest Day (shift from 19:00 to 4:00)
-    {'employee': '22831242', 'time': '2024-02-07 19:00:00', 'log_type': 'IN'},  # Night shift start before Chinese New Year (Wednesday)
-    {'employee': '22831242', 'time': '2024-02-08 04:00:00', 'log_type': 'OUT'},  # Night shift end (Thursday)
+    {'employee': '22831242', 'time': '2023-02-07 19:00:00', 'log_type': 'IN'},  # Night shift start before Chinese New Year (Tuesday)
+    {'employee': '22831242', 'time': '2023-02-08 04:00:00', 'log_type': 'OUT'},  # Night shift end (Wednesday)
 
     # Combined Special Non-Working Holiday and Rest Day Overtime with Attendance on the Previous Day
-    {'employee': '22831242', 'time': '2024-02-08 19:00:00', 'log_type': 'IN'},  # Night shift start on Chinese New Year (Thursday)
-    {'employee': '22831242', 'time': '2024-02-09 06:00:00', 'log_type': 'OUT'},  # Night shift end with 2 hours overtime (Friday)
+    {'employee': '22831242', 'time': '2023-02-08 19:00:00', 'log_type': 'IN'},  # Night shift start on Chinese New Year (Wednesday)
+    {'employee': '22831242', 'time': '2023-02-09 06:00:00', 'log_type': 'OUT'},  # Night shift end with 2 hours overtime (Thursday)
     ]
 
 
