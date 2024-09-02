7f974996d8a6abf0f8cc1d0fb87b17b089373325
Ranz Manalo <manaloranzelle10@gmail.com>
2024-09-02 09:28:32 +0800

    g

--- delete_attendance.sql
+++ delete_attendance.sql
@@ -9,4 +9,4 @@ DELETE FROM `tabAttendance`
 WHERE `shift` = 'Row3RanzTest';
 
 
-DELETE FROM `tabAttendance` WHERE `shift` = 'FOR TESTING OF OT';
+DELETE FROM `tabAttendance` WHERE `shift` = 'TESTING NIGHT DIFF';
